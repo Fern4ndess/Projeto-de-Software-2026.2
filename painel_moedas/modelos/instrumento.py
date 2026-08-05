78f0b634d172380d40b36391f19441b74353826b
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-08-05 11:35:05 -0300

    Remove docstring from Instrumento class
    Removed docstring for the Instrumento class.

--- painel_moedas/modelos/instrumento.py
+++ painel_moedas/modelos/instrumento.py
@@ -1,10 +1,6 @@
 from abc import ABC, abstractmethod
 
 class Instrumento(ABC):
-    """
-    RF3 [A/H]
-    Classe abstrata para qualquer instrumento financeiro.
-    """
 
     def __init__(self, codigo):
         self._codigo = codigo
