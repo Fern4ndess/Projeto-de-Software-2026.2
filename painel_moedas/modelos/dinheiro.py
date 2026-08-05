3025cbcabf88a55a025ea23b100aaee476ffdfd2
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-08-05 11:34:31 -0300

    Refactor Dinheiro class to support addition and string representation

--- painel_moedas/modelos/dinheiro.py
+++ painel_moedas/modelos/dinheiro.py
@@ -1,7 +1,7 @@
 class Dinheiro:
 
     def __init__(self, valor, moeda):
-        self.__valor = float(valor)
+        self.__valor = valor
         self.__moeda = moeda.upper()
 
     @property
@@ -11,3 +11,19 @@ class Dinheiro:
     @property
     def moeda(self):
         return self.__moeda
+
+    def __add__(self, outro):
+
+        if not isinstance(outro, Dinheiro):
+            raise TypeError("Operação permitida apenas entre objetos Dinheiro.")
+
+        if self.__moeda != outro.__moeda:
+            raise ValueError("Não é permitido somar moedas diferentes.")
+
+        return Dinheiro(
+            self.__valor + outro.__valor,
+            self.__moeda
+        )
+
+    def __str__(self):
+        return f"{self.__valor:.2f} {self.__moeda}"
