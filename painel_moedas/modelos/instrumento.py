16ec529ba2d3702151f5e9ddda5c6d763c25b1ee
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-08-05 11:34:50 -0300

    Add abstract Instrumento class for financial instruments

--- painel_moedas/modelos/instrumento.py
+++ painel_moedas/modelos/instrumento.py
@@ -1,1 +1,18 @@
+from abc import ABC, abstractmethod
 
+class Instrumento(ABC):
+    """
+    RF3 [A/H]
+    Classe abstrata para qualquer instrumento financeiro.
+    """
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+
+    @property
+    def codigo(self):
+        return self._codigo
+
+    @abstractmethod
+    def calcular_volatilidade(self):
+        pass
