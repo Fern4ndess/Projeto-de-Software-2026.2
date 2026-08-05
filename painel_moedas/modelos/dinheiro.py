c24ffacc145c865551a3af2b6ed3a016090cb5d2
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-08-05 11:31:48 -0300

    Update dinheiro.py

--- painel_moedas/modelos/dinheiro.py
+++ painel_moedas/modelos/dinheiro.py
@@ -1,1 +1,13 @@
+class Dinheiro:
 
+    def __init__(self, valor, moeda):
+        self.__valor = float(valor)
+        self.__moeda = moeda.upper()
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @property
+    def moeda(self):
+        return self.__moeda
