9ac3b07371a2b62907bccc30ecd88ab1d9c8ef2e
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-08-12 11:43:17 -0300

    Update instrumento.py

--- painel_moedas/modelos/instrumento.py
+++ painel_moedas/modelos/instrumento.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
+
 class Instrumento(ABC):
 
     def __init__(self, codigo):
-        self._codigo = codigo
+        self._codigo = codigo.upper()
 
     @property
     def codigo(self):
@@ -11,4 +12,8 @@ class Instrumento(ABC):
 
     @abstractmethod
     def calcular_volatilidade(self):
+        pass
+
+    @abstractmethod
+    def formatar_valor(self, valor):
         pass
