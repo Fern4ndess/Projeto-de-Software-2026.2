3515fa4b706d5f330fd986c7785a5177a11ac442
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-09-10 15:22:56 -0300

    Update cotacao.py
    alteração e melhorias considerando todos os requisitos (antes de futura aplicação de API)

--- modelos/cotacao.py
+++ modelos/cotacao.py
@@ -1,28 +1,16 @@
+from dataclasses import dataclass
 from datetime import datetime
 
+
+@dataclass(frozen=True)
 class Cotacao:
-
-    def __init__(self, moeda_origem, moeda_destino, valor, horario):
-        self.__moeda_origem = moeda_origem
-        self.__moeda_destino = moeda_destino
-        self.__valor = valor
-        self.__horario = horario
-
-    @property
-    def moeda_origem(self):
-        return self.__moeda_origem
-
-    @property
-    def moeda_destino(self):
-        return self.__moeda_destino
-
-    @property
-    def valor(self):
-        return self.__valor
-
-    @property
-    def horario(self):
-        return self.__horario
+    moeda_origem: str
+    moeda_destino: str
+    valor: object
+    horario: datetime
 
     def __str__(self):
-        return f"{self.__moeda_origem} -> {self.__moeda_destino}: {self.__valor} ({self.__horario})"
+        return (
+            f"{self.moeda_origem} -> {self.moeda_destino}: "
+            f"{self.valor} ({self.horario})"
+        )
